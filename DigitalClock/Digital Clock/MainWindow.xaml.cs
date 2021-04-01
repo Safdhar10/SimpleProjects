5804ac41b5b152d0cc690a9027d16af9cb338e91
Safdhar10 <safdhard@gmail.com>
2021-04-01 16:04:03 +0530

    Digital Clock

---
+++ DigitalClock/Digital Clock/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Digital_Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> TimeZones = new List<string>();
+        Time GetTime = new Time();
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddTimeZone();
+            Region.ItemsSource = TimeZones;
+            Region.SelectedItem = "India Standard Time";
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+        private void AddTimeZone()
+        {
+            var AllTimeZones = TimeZoneInfo.GetSystemTimeZones();
+            foreach (var item in AllTimeZones)
+            {
+                TimeZones.Add(item.StandardName);
+            }
+        }
+        private void dispatcherTimer_tick(object sender, EventArgs e)
+        {
+            GetTime = new Time();
+            HourMinute.Content = GetTime.Hour(Region.SelectedItem.ToString())+":"+GetTime.Minute(Region.SelectedItem.ToString());
+            Seconds.Content = GetTime.Second(Region.SelectedItem.ToString());
+            TT.Content = GetTime.AmORPm(Region.SelectedItem.ToString());
+        }
+
+        private void Region_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            HourMinute.Content = GetTime.Hour(Region.SelectedItem.ToString()) + ":" + GetTime.Minute(Region.SelectedItem.ToString());
+            Seconds.Content = GetTime.Second(Region.SelectedItem.ToString());
+            TT.Content = GetTime.AmORPm(Region.SelectedItem.ToString());
+        }
+    }
+}
---
+++ DigitalClock/Digital Clock/Time.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+using TimeZoneConverter;
+
+namespace Digital_Clock
+{
+    class Time
+    {
+        public Dictionary<string, string> RegionTimeDetails = new Dictionary<string, string>();
+        private static TimeZoneInfo tzi; 
+        DateTime dt;
+        public void TimeZoneConverter(String TimeZone)
+        {
+            tzi = TZConvert.GetTimeZoneInfo(TimeZone);
+            dt = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzi);
+        }
+        public string Hour(string Zone)
+        {
+            TimeZoneConverter(Zone);
+            //string _hour = dt.Hour + ":";
+
+            //if (dt.Hour<=9)
+            //{
+            //    _hour = "0" + dt.Hour +":";
+            //}
+            //return _hour;
+            return dt.ToString("hh");
+        }
+        public string Minute(string Zone)
+        {
+            TimeZoneConverter(Zone);
+            //string _minute = dt.Minute.ToString();
+            //if (dt.Minute <= 9)
+            //{
+            //    _minute = "0" + dt.Minute.ToString();
+            //}
+            //return _minute;
+            return dt.ToString("mm");
+        }
+        public string Second(String Zone)
+        {
+            TimeZoneConverter(Zone);
+            //string _second = dt.Second.ToString(); 
+            //if (dt.Second <= 9)
+            //{
+            //    _second = "0" + dt.Second.ToString();
+            //}
+            //return _second;
+            return dt.ToString("ss");
+        }
+        public string AmORPm(String Zone)
+        {
+            TimeZoneConverter(Zone);
+            return dt.ToString("tt");
+        }
+    }
+}
