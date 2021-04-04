34892a9f9188072248bc489717da29a6c30c8312
Safdhar10 <safdhard@gmail.com>
2021-04-04 22:16:24 +0530

    countdown timer

---
+++ CountdownTimer/CountdownTimer/CountdownPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+namespace CountdownTimer
+{
+    /// <summary>
+    /// Interaction logic for CountdownPage.xaml
+    /// </summary>
+    public partial class CountdownPage : Page
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        Stopwatch _stopWatch = new Stopwatch();
+        
+        public CountdownPage()
+        {
+            InitializeComponent();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_tick);
+            dispatcherTimer.Interval = new TimeSpan(0,0,0,0,1);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_tick(object sender, EventArgs e)
+        {
+            if(_stopWatch.IsRunning)
+            {
+                TimeSpan ts = _stopWatch.Elapsed;
+                string currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds/10 );
+                Time.Content = currentTime;
+            }
+        }
+
+        private void strtBtn(object sender, RoutedEventArgs e)
+        {
+            _stopWatch.Start();
+            startPanel.Visibility = Visibility.Hidden;
+            pausePanel.Visibility = Visibility.Visible;
+        }
+        private void pauseBtn(object sender, RoutedEventArgs e)
+        {
+            _stopWatch.Stop();
+            pausePanel.Visibility = Visibility.Hidden;
+            stopPanel.Visibility = Visibility.Visible;
+        }
+
+        private void stopBtn(object sender, RoutedEventArgs e)
+        {
+            _stopWatch.Reset();
+            Time.Content = "00:00:00";
+            stopPanel.Visibility = Visibility.Hidden;
+            startPanel.Visibility = Visibility.Visible;
+        }
+
+        private void resumeBtn(object sender, RoutedEventArgs e)
+        {
+            _stopWatch.Start();
+        }
+
+    }
+}
---
+++ CountdownTimer/CountdownTimer/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CountdownTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            frame.Navigate(new Uri("CountdownPage.xaml", UriKind.Relative));
+        }
+
+       
+
+        private void Stopwatch(object sender, RoutedEventArgs e)
+        {
+            BrushConverter bc = new BrushConverter();
+            TimerButton.Background = new SolidColorBrush(Colors.Bisque);
+            StopWatchButton.Background = (Brush)bc.ConvertFrom("#caf7e3");
+
+        }
+
+        private void Timer(object sender, RoutedEventArgs e)
+        {
+            BrushConverter bc = new BrushConverter();
+            StopWatchButton.Background = new SolidColorBrush(Colors.Bisque);
+            TimerButton.Background = (Brush)bc.ConvertFrom("#caf7e3");
+        }
+    }
+}
