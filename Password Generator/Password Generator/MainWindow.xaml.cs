d2537e6580df2c4a999d6f069378abc68c2842ea
Safdhar10 <safdhard@gmail.com>
2021-04-05 21:12:01 +0530

    Password Genrator

---
+++ Password Generator/Password Generator/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Web;
+using System.Security;
+
+namespace Password_Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RandomGenerator RG = new RandomGenerator();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LetterText.Text = "3";
+            RG.RandomPasswordGenerator(10, 3, 4, 2, 1);
+        }
+
+        private void UPCheck(object sender, RoutedEventArgs e)
+        {
+            if(UpCheckButton.IsChecked==true)
+            {
+                UPText.IsEnabled = true;
+            }
+            else
+            {
+                UPText.Text = "0";
+                Total();
+                UPText.IsEnabled = false;
+            }
+        }
+
+        private void SYMCheck(object sender, RoutedEventArgs e)
+        {
+            if(SymButton.IsChecked==true)
+            {
+                SymText.IsEnabled = true;
+            }
+            else
+            {
+                SymText.Text = "0";
+                Total();
+                SymText.IsEnabled = false;
+            }
+        }
+
+        private void NUMCheck(object sender, RoutedEventArgs e)
+        {
+            if (NumCheckButton.IsChecked == true)
+            {
+                NumText.IsEnabled = true;
+            }
+            else
+            {
+                NumText.Text = "0";
+                Total();
+                NumText.IsEnabled = false;
+            }
+        }
+
+        private bool IsValid(int num)
+        {
+
+            if (num >= 0 && num <= 256)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        private void LetterText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TotalLength.Text = (Convert.ToInt32(LetterText.Text)).ToString();
+        }
+        private void UPText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (UpCheckButton.IsChecked == true)
+                //TotalLength.Text = (Convert.ToInt32(LetterText.Text)+Convert.ToInt32(UPText.Text)).ToString();
+                Total();
+            }
+
+        private void NumText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (NumCheckButton.IsChecked == true)
+                //TotalLength.Text = (Convert.ToInt32(LetterText.Text) + Convert.ToInt32(NumText.Text) + Convert.ToInt32(UPText.Text)).ToString();
+                Total();
+        }
+
+        private void SymText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (SymButton.IsChecked == true)
+                //TotalLength.Text = (Convert.ToInt32(LetterText.Text) + Convert.ToInt32(NumText.Text) + Convert.ToInt32(SymText.Text) + Convert.ToInt32(UPText.Text)).ToString();
+                Total();
+        }
+        private void Total()
+        {
+            TotalLength.Text = (Convert.ToInt32(LetterText.Text) + Convert.ToInt32(NumText.Text) + Convert.ToInt32(SymText.Text) + Convert.ToInt32(UPText.Text)).ToString();
+        }
+
+        private void GeneratePass(object sender, RoutedEventArgs e)
+        {
+            string _getPassword=RG.RandomPasswordGenerator(Convert.ToInt32(TotalLength.Text), Convert.ToInt32(NumText.Text), Convert.ToInt32(LetterText.Text), Convert.ToInt32(UPText.Text), Convert.ToInt32(SymText.Text));
+            Result.Text = _getPassword;
+        }
+        private void TotalLength_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!IsValid(Convert.ToInt32(TotalLength.Text)))
+                MessageBox.Show("Please Enter Password Length Between 256 Characters");
+            
+        }
+
+        private void CopyToClip(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(Result.Text);
+
+        }
+    }
+}
---
+++ Password Generator/Password Generator/RandomGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Generator
+{
+    class RandomGenerator
+    {
+        Random r = new Random();
+        private char RadomLowerCaseChar()
+        {
+            return Convert.ToChar(r.Next(97, 122));
+        }
+        private char RandomUpperCaseChar()
+        {
+            return Convert.ToChar(r.Next(65, 90));
+        }
+        private int RandomInt()
+        {
+            return r.Next(1, 9);
+        }
+        private char RandomSymbol()
+        {
+            return Convert.ToChar(r.Next(33, 47));
+        }
+        public string RandomPasswordGenerator(int TotalLength,int NumberCount,int LetterCount,int UpperCaseCount,int SymbolCount)
+        {
+            string[] PasswordArray = new string[TotalLength];
+            StringBuilder Password = new StringBuilder();
+            for(int i=0;i<TotalLength;i++)
+            {
+                if(LetterCount>0)
+                {
+                    // PasswordArray[i] = RandomInt().ToString();
+                    Password.Append(RadomLowerCaseChar().ToString());
+                    LetterCount--;
+                }
+                else if(UpperCaseCount>0)
+                {
+                    //PasswordArray[i] = RadomLowerCaseChar().ToString(); 
+                    Password.Append(RandomUpperCaseChar().ToString());
+                    UpperCaseCount--;
+                }
+                else if(NumberCount>0)
+                {
+                    //PasswordArray[i] = RandomInt().ToString();
+                    Password.Append(RandomInt().ToString());
+                    NumberCount--;
+                }
+                else
+                {
+                    //PasswordArray[i] = RandomSymbol().ToString();
+                    Password.Append(RandomSymbol().ToString());
+                    SymbolCount--;
+                }
+            }
+
+            for (int i = 0; i < Password.Length - 1; i++)
+            {
+                int j = r.Next(i, TotalLength);
+                char temp = Password[i];
+                Password[i] = Password[j];
+                Password[j] = temp;
+            }
+            return Password.ToString();
+        }
+    }
+}
