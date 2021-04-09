49e7a12144e39da3dafa109f8b9abdc9a1448f6c
Safdhar10 <safdhard@gmail.com>
2021-04-09 17:31:25 +0530

    Changes

--- Digital Calculator/Digital Calculator/MainWindow.xaml.cs
+++ Digital Calculator/Digital Calculator/MainWindow.xaml.cs
@@ -24,5 +24,75 @@ namespace Digital_Calculator
         {
             InitializeComponent();
         }
+
+        private void WriteContent(string input)
+        {
+            DisplayBlock.Text += input;
+        }
+        private void ZeroBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(ZeroBtn.Content.ToString());
+        }
+
+        private void OneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(OneBtn.Content.ToString());
+        }
+
+        private void TwoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(TwoBtn.Content.ToString());
+        }
+
+        private void ThreeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(ThreeBtn.Content.ToString());
+        }
+
+        private void XSquareBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void fourBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(fourBtn.Content.ToString());
+        }
+
+        private void fiveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(fiveBtn.Content.ToString());
+        }
+
+        private void sixBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(sixBtn.Content.ToString());
+        }
+
+        private void xfactorBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sevenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(sevenBtn.Content.ToString());
+        }
+
+        private void eightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(eightBtn.Content.ToString());
+        }
+
+        private void nineBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WriteContent(nineBtn.Content.ToString());
+        }
+
+        private void TextBlock_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            bool check=
+            e.Handled = 
+        }
     }
 }
